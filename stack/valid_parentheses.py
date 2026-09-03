db9e7b9554269c29ae478da81389dba608db427e
SAKTHISPARROW <sakthithirunavu07@gmail.com>
2026-09-03 23:33:47 +0530

    Add: Valid Parentheses Problem (LeetCode #20)

---
+++ stack/valid_parentheses.py
@@ -0,0 +1,36 @@
+# Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+# An input string is valid if:
+
+#     Open brackets must be closed by the same type of brackets.
+#     Open brackets must be closed in the correct order.
+#     Every close bracket has a corresponding open bracket of the same type.
+
+# Example 1:
+
+# Input: s = "()"
+# Output: true
+# Link: https://leetcode.com/problems/valid-parentheses/description/
+
+# Approach 1: Stack
+
+def is_valid(s: str) -> bool:
+    stack = []
+    close_to_open = { ')' : '(', '}' : '{', ']' : '['}
+    for c in s:
+        if c in close_to_open:
+            if stack and stack[-1] == close_to_open[c]:
+                stack.pop()
+            else:
+                return False
+        else:
+            stack.append(c)
+
+    return True if not stack else False
+
+print(is_valid("(("))
+
+# Time & Space Complexity
+
+#     Time complexity: O(n)
+#     Space complexity: O(n)
